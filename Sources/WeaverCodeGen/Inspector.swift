a3cd5540322e4cbae391aaeecb59a82a2c307ba8
Theo <theophane.rupin@gmail.com>
2018-05-06 09:58:26 -0700

    Moved Inspector's nested types to regular types

--- Sources/WeaverCodeGen/Inspector.swift
+++ Sources/WeaverCodeGen/Inspector.swift
@@ -8,12 +8,14 @@
 import Foundation
 import Weaver
 
+// MARK: - Inspector
+
 public final class Inspector {
 
     private let graph = Graph()
 
-    private lazy var resolutionCache = Set<Inspector.ResolutionCacheIndex>()
-    private lazy var buildCache = Set<Inspector.BuildCacheIndex>()
+    private lazy var resolutionCache = Set<ResolutionCacheIndex>()
+    private lazy var buildCache = Set<BuildCacheIndex>()
     
     public init(syntaxTrees: [Expr]) throws {
         try buildGraph(from: syntaxTrees)
@@ -27,87 +29,86 @@ public final class Inspector {
     }
 }
 
-private extension Inspector {
+// MARK: - Graph Objects
+
+private final class Graph {
+    private var resolversByName = [String: Resolver]()
+    private var resolversByType = [String: Resolver]()
     
-    final class Graph {
-        private var resolversByName = [String: Resolver]()
-        private var resolversByType = [String: Resolver]()
-        
-        lazy var dependencies: [Dependency] = {
-            var allDependencies = [Dependency]()
-            allDependencies.append(contentsOf: resolversByName.values.flatMap { $0.dependencies.values })
-            allDependencies.append(contentsOf: resolversByType.values.flatMap { $0.dependencies.values })
- 
-            var filteredDependencies = Set<Dependency>()
-            return allDependencies.filter {
-                if filteredDependencies.contains($0) {
-                    return false
-                }
-                filteredDependencies.insert($0)
-                return true
+    lazy var dependencies: [Dependency] = {
+        var allDependencies = [Dependency]()
+        allDependencies.append(contentsOf: resolversByName.values.flatMap { $0.dependencies.values })
+        allDependencies.append(contentsOf: resolversByType.values.flatMap { $0.dependencies.values })
+
+        var filteredDependencies = Set<Dependency>()
+        return allDependencies.filter {
+            if filteredDependencies.contains($0) {
+                return false
             }
-        }()
-    }
-
-    final class Resolver {
-        let typeName: String?
-        var dependencies: [DependencyIndex: Dependency] = [:]
-        var dependents: [Resolver] = []
-        
-        init(typeName: String? = nil) {
-            self.typeName = typeName
+            filteredDependencies.insert($0)
+            return true
         }
-    }
-    
-    struct DependencyIndex {
-        let typeName: String?
-        let name: String
-    }
-    
-    final class Dependency {
-        let name: String
-        let scope: Scope?
-        let isCustom: Bool
-        let associatedResolver: Resolver
-        let dependentResovler: Resolver
+    }()
+}
 
-        let line: Int
-        let file: String
+private final class Resolver {
+    let typeName: String?
+    var dependencies: [DependencyIndex: Dependency] = [:]
+    var dependents: [Resolver] = []
+    
+    init(typeName: String? = nil) {
+        self.typeName = typeName
+    }
+}
 
-        init(name: String,
-             scope: Scope? = nil,
-             isCustom: Bool,
-             line: Int,
-             file: String,
-             associatedResolver: Resolver,
-             dependentResovler: Resolver) {
-            self.name = name
-            self.scope = scope
-            self.isCustom = isCustom
-            self.line = line
-            self.file = file
-            self.associatedResolver = associatedResolver
-            self.dependentResovler = dependentResovler
-        }
+private struct DependencyIndex {
+    let typeName: String?
+    let name: String
+}
+
+private final class Dependency {
+    let name: String
+    let scope: Scope?
+    let isCustom: Bool
+    let associatedResolver: Resolver
+    let dependentResovler: Resolver
+
+    let line: Int
+    let file: String
+
+    init(name: String,
+         scope: Scope? = nil,
+         isCustom: Bool,
+         line: Int,
+         file: String,
+         associatedResolver: Resolver,
+         dependentResovler: Resolver) {
+        self.name = name
+        self.scope = scope
+        self.isCustom = isCustom
+        self.line = line
+        self.file = file
+        self.associatedResolver = associatedResolver
+        self.dependentResovler = dependentResovler
     }
-    
-    struct ResolutionCacheIndex {
-        let resolver: Resolver
-        let dependencyIndex: DependencyIndex
-    }
-    
-    struct BuildCacheIndex {
-        let resolver: Resolver
-        let scope: Scope?
-    }
+}
+
+private struct ResolutionCacheIndex {
+    let resolver: Resolver
+    let dependencyIndex: DependencyIndex
+}
+
+private struct BuildCacheIndex {
+    let resolver: Resolver
+    let scope: Scope?
 }
 
 // MARK: - Graph
 
-extension Inspector.Graph {
+extension Graph {
     
     func insertResolver(with registerAnnotation: RegisterAnnotation) {
-        let resolver = Inspector.Resolver(typeName: registerAnnotation.typeName)
+        let resolver = Resolver(typeName: registerAnnotation.typeName)
         resolversByName[registerAnnotation.name] = resolver
         resolversByType[registerAnnotation.typeName] = resolver
     }
@@ -116,18 +117,18 @@ extension Inspector.Graph {
         if resolversByName[referenceAnnotation.name] != nil {
             return
         }
-        resolversByName[referenceAnnotation.name] = Inspector.Resolver()
+        resolversByName[referenceAnnotation.name] = Resolver()
     }
 
-    func resolver(named name: String) -> Inspector.Resolver? {
+    func resolver(named name: String) -> Resolver? {
         return resolversByName[name]
     }
     
-    func resolver(typed type: String) -> Inspector.Resolver {
+    func resolver(typed type: String) -> Resolver {
         if let resolver = resolversByType[type] {
             return resolver
         }
-        let resolver = Inspector.Resolver(typeName: type)
+        let resolver = Resolver(typeName: type)
         resolversByType[type] = resolver
         return resolver
     }
@@ -215,14 +216,14 @@ private extension Inspector {
     }
 }
 
-private extension Inspector.Dependency {
+private extension Dependency {
     
-    convenience init(dependentResolver: Inspector.Resolver,
+    convenience init(dependentResolver: Resolver,
                      registerAnnotation: TokenBox<RegisterAnnotation>,
                      scopeAnnotation: ScopeAnnotation? = nil,
                      customRefAnnotation: CustomRefAnnotation?,
                      fileName: String,
-                     graph: Inspector.Graph) throws {
+                     graph: Graph) throws {
 
         guard let associatedResolver = graph.resolver(named: registerAnnotation.value.name) else {
             throw InspectorError.invalidGraph(line: registerAnnotation.line,
@@ -241,11 +242,11 @@ private extension Inspector.Dependency {
                   dependentResovler: dependentResolver)
     }
     
-    convenience init(dependentResolver: Inspector.Resolver,
+    convenience init(dependentResolver: Resolver,
                      referenceAnnotation: TokenBox<ReferenceAnnotation>,
                      customRefAnnotation: CustomRefAnnotation?,
                      fileName: String,
-                     graph: Inspector.Graph) throws {
+                     graph: Graph) throws {
 
         guard let associatedResolver = graph.resolver(named: referenceAnnotation.value.name) else {
             throw InspectorError.invalidGraph(line: referenceAnnotation.line,
@@ -264,9 +265,9 @@ private extension Inspector.Dependency {
     }
 }
 
-private extension Inspector.Resolver {
+private extension Resolver {
     
-    func update(with children: [Expr], fileName: String, graph: Inspector.Graph) throws {
+    func update(with children: [Expr], fileName: String, graph: Graph) throws {
 
         var registerAnnotations: [TokenBox<RegisterAnnotation>] = []
         var referenceAnnotations: [TokenBox<ReferenceAnnotation>] = []
@@ -298,24 +299,24 @@ private extension Inspector.Resolver {
         }
         
         for registerAnnotation in registerAnnotations {
-            let dependency = try Inspector.Dependency(dependentResolver: self,
+            let dependency = try Dependency(dependentResolver: self,
                                                       registerAnnotation: registerAnnotation,
                                                       scopeAnnotation: scopeAnnotations[registerAnnotation.value.name],
                                                       customRefAnnotation: customRefAnnotations[registerAnnotation.value.name],
                                                       fileName: fileName,
                                                       graph: graph)
-            let index = Inspector.DependencyIndex(typeName: dependency.associatedResolver.typeName, name: dependency.name)
+            let index = DependencyIndex(typeName: dependency.associatedResolver.typeName, name: dependency.name)
             dependencies[index] = dependency
             dependency.associatedResolver.dependents.append(self)
         }
         
         for referenceAnnotation in referenceAnnotations {
-            let dependency = try Inspector.Dependency(dependentResolver: self,
+            let dependency = try Dependency(dependentResolver: self,
                                                       referenceAnnotation: referenceAnnotation,
                                                       customRefAnnotation: customRefAnnotations[referenceAnnotation.value.name],
                                                       fileName: fileName,
                                                       graph: graph)
-            let index = Inspector.DependencyIndex(typeName: dependency.associatedResolver.typeName, name: dependency.name)
+            let index = DependencyIndex(typeName: dependency.associatedResolver.typeName, name: dependency.name)
             dependencies[index] = dependency
             dependency.associatedResolver.dependents.append(self)
         }
@@ -324,9 +325,9 @@ private extension Inspector.Resolver {
 
 // MARK: - Resolution Check
 
-private extension Inspector.Dependency {
+private extension Dependency {
     
-    func resolve(with cache: inout Set<Inspector.ResolutionCacheIndex>) throws {
+    func resolve(with cache: inout Set<ResolutionCacheIndex>) throws {
         guard isReference && !isCustom else {
             return
         }
@@ -339,7 +340,7 @@ private extension Inspector.Dependency {
                                               underlyingError: .unresolvableDependency)
         }
         
-        let index = Inspector.DependencyIndex(typeName: associatedResolver.typeName, name: name)
+        let index = DependencyIndex(typeName: associatedResolver.typeName, name: name)
         for dependent in dependentResovler.dependents {
             do {
                 try dependent.resolveDependency(index: index, cache: &cache)
@@ -354,21 +355,21 @@ private extension Inspector.Dependency {
     }
 }
 
-private extension Inspector.Resolver {
+private extension Resolver {
     
-    func resolveDependency(index: Inspector.DependencyIndex, cache: inout Set<Inspector.ResolutionCacheIndex>) throws {
-        let cacheIndex = Inspector.ResolutionCacheIndex(resolver: self, dependencyIndex: index)
+    func resolveDependency(index: DependencyIndex, cache: inout Set<ResolutionCacheIndex>) throws {
+        let cacheIndex = ResolutionCacheIndex(resolver: self, dependencyIndex: index)
         guard !cache.contains(cacheIndex) else {
             return
         }
 
-        var visitedResolvers = Set<Inspector.Resolver>()
+        var visitedResolvers = Set<Resolver>()
         try resolveDependency(index: index, visitedResolvers: &visitedResolvers)
         
         cache.insert(cacheIndex)
     }
     
-    private func resolveDependency(index: Inspector.DependencyIndex, visitedResolvers: inout Set<Inspector.Resolver>) throws {
+    private func resolveDependency(index: DependencyIndex, visitedResolvers: inout Set<Resolver>) throws {
         if visitedResolvers.contains(self) {
             throw InspectorAnalysisError.cyclicDependency
         }
@@ -393,10 +394,10 @@ private extension Inspector.Resolver {
 
 // MARK: - Build Check
 
-private extension Inspector.Dependency {
+private extension Dependency {
     
-    func build(with buildCache: inout Set<Inspector.BuildCacheIndex>) throws {
-        let buildCacheIndex = Inspector.BuildCacheIndex(resolver: associatedResolver, scope: scope)
+    func build(with buildCache: inout Set<BuildCacheIndex>) throws {
+        let buildCacheIndex = BuildCacheIndex(resolver: associatedResolver, scope: scope)
         guard !buildCache.contains(buildCacheIndex) else {
             return
         }
@@ -410,14 +411,14 @@ private extension Inspector.Dependency {
             return
         }
         
-        var visitedResolvers = Set<Inspector.Resolver>()
+        var visitedResolvers = Set<Resolver>()
         try associatedResolver.buildDependencies(from: self, visitedResolvers: &visitedResolvers)
     }
 }
 
-private extension Inspector.Resolver {
+private extension Resolver {
     
-    func buildDependencies(from sourceDependency: Inspector.Dependency, visitedResolvers: inout Set<Inspector.Resolver>) throws {
+    func buildDependencies(from sourceDependency: Dependency, visitedResolvers: inout Set<Resolver>) throws {
 
         if visitedResolvers.contains(self) {
             throw InspectorError.invalidGraph(line: sourceDependency.line,
@@ -437,7 +438,7 @@ private extension Inspector.Resolver {
 
 // MARK: - Utils
 
-private extension Inspector.Dependency {
+private extension Dependency {
     
     var isReference: Bool {
         return scope == nil
@@ -446,60 +447,60 @@ private extension Inspector.Dependency {
 
 // MARK: - Hashable
 
-extension Inspector.Resolver: Hashable {
+extension Resolver: Hashable {
     
     var hashValue: Int {
         return ObjectIdentifier(self).hashValue
     }
     
-    static func ==(lhs: Inspector.Resolver, rhs: Inspector.Resolver) -> Bool {
+    static func ==(lhs: Resolver, rhs: Resolver) -> Bool {
         return ObjectIdentifier(lhs) == ObjectIdentifier(rhs)
     }
 }
 
-extension Inspector.Dependency: Hashable {
+extension Dependency: Hashable {
 
     var hashValue: Int {
         return ObjectIdentifier(self).hashValue
     }
     
-    static func ==(lhs: Inspector.Dependency, rhs: Inspector.Dependency) -> Bool {
+    static func ==(lhs: Dependency, rhs: Dependency) -> Bool {
         return ObjectIdentifier(lhs) == ObjectIdentifier(rhs)
     }
 }
 
-extension Inspector.DependencyIndex: Hashable {
+extension DependencyIndex: Hashable {
     
     var hashValue: Int {
         return (typeName ?? "").hashValue ^ name.hashValue
     }
     
-    static func ==(lhs: Inspector.DependencyIndex, rhs: Inspector.DependencyIndex) -> Bool {
+    static func ==(lhs: DependencyIndex, rhs: DependencyIndex) -> Bool {
         guard lhs.name == rhs.name else { return false }
         guard lhs.typeName == rhs.typeName else { return false }
         return true
     }
 }
 
-extension Inspector.ResolutionCacheIndex: Hashable {
+extension ResolutionCacheIndex: Hashable {
 
     var hashValue: Int {
         return resolver.hashValue ^ dependencyIndex.hashValue
     }
     
-    static func ==(lhs: Inspector.ResolutionCacheIndex, rhs: Inspector.ResolutionCacheIndex) -> Bool {
+    static func ==(lhs: ResolutionCacheIndex, rhs: ResolutionCacheIndex) -> Bool {
         guard lhs.resolver == rhs.resolver else { return false }
         guard lhs.dependencyIndex == rhs.dependencyIndex else { return false }
         return true
     }
 }
 
-extension Inspector.BuildCacheIndex: Hashable {
+extension BuildCacheIndex: Hashable {
     var hashValue: Int {
         return resolver.hashValue ^ (scope?.hashValue ?? 0)
     }
     
-    static func ==(lhs: Inspector.BuildCacheIndex, rhs: Inspector.BuildCacheIndex) -> Bool {
+    static func ==(lhs: BuildCacheIndex, rhs: BuildCacheIndex) -> Bool {
         guard lhs.resolver == rhs.resolver else { return false }
         guard lhs.scope == rhs.scope else { return false }
         return true
