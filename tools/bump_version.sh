6415c9277ca2362f6770f96956bad48bcb330290
Theo <theophane.rupin@gmail.com>
2018-05-21 10:17:40 -0700

    Added bump_version.sh

---
+++ tools/bump_version.sh
@@ -0,0 +1,17 @@
+current_version=`/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" Weaver.xcodeproj/WeaverCodeGen_Info.plist`
+
+next_version=$1
+if [ -z $next_version ]; then
+	next_version=${current_version%.*}.$((${current_version##*.}+1))
+fi
+
+echo $next_version
+
+sed -i '' "s/${current_version//./\\.}/$next_version/g" \
+Weaver.xcodeproj/Weaver_Info.plist \
+Weaver.xcodeproj/WeaverCodeGen_Info.plist \
+Weaver.podspec \
+README.md \
+Resources/dependency_resolver.stencil
+
+git commit -am "Bump version to $next_version"
