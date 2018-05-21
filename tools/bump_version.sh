fc9d9d4875709bb649f189a8c2f06e8a0c70aeee
Theo <theophane.rupin@gmail.com>
2018-05-21 10:58:31 -0700

    Fixed readme version bump

--- tools/bump_version.sh
+++ tools/bump_version.sh
@@ -11,7 +11,10 @@ sed -i '' "s/${current_version//./\\.}/$next_version/g" \
 Weaver.xcodeproj/Weaver_Info.plist \
 Weaver.xcodeproj/WeaverCodeGen_Info.plist \
 Weaver.podspec \
-README.md \
-Resources/dependency_resolver.stencil
+Resources/dependency_resolver.stencil \
+Tests/WeaverCodeGenTests/GeneratorTests.swift
+
+previous_version=${current_version%.*}.$((${current_version##*.}-1))
+sed -i '' "s/${previous_version//./\\.}/$current_version/g" README.md
 
 git commit -am "Bump version to $next_version"
