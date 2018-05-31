9a8f38d242e6933a5f08f25b226a032436078754
Theo <theophane.rupin@gmail.com>
2018-05-31 10:52:53 -0700

    Added formula to bump version script

--- tools/bump_version.sh
+++ tools/bump_version.sh
@@ -15,7 +15,13 @@ Resources/dependency_resolver.stencil \
 Tests/WeaverCodeGenTests/GeneratorTests.swift \
 Sources/WeaverCommand/main.swift
 
+release_tar="https://github.com/scribd/Weaver/archive/$current_version.tar.gz"
+sha=$(curl -L -s $release_tar | shasum -a 256 | sed 's/ .*//')
+sed -i '' "s|\(sha256 \"\)\(.*\)\(\"\)|\1$sha\3|" $(brew --repo homebrew/core)/Formula/weaver.rb
+
 previous_version=${current_version%.*}.$((${current_version##*.}-1))
-sed -i '' "s/${previous_version//./\\.}/$current_version/g" README.md
+sed -i '' "s/${previous_version//./\\.}/$current_version/g" \
+README.md \
+$(brew --repo homebrew/core)/Formula/weaver.rb
 
 git commit -am "Bump version to $next_version"
