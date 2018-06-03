9fced591750045be29ae22250ba038310bc02979
Theo <theophane.rupin@gmail.com>
2018-06-03 08:40:32 -0700

    Added compactMap retrocompat

---
+++ Sources/WeaverCodeGen/shim-swift-4.1.swift
@@ -0,0 +1,17 @@
+//
+//  shim-swift-4.2.swift
+//  WeaverCodeGen
+//
+//  Created by Théophane Rupin on 6/3/18.
+//
+
+import Foundation
+
+#if swift(>=4.1)
+#else
+extension Collection {
+    public func compactMap<ElementOfResult>(_ transform: (Element) throws -> ElementOfResult?) rethrows -> [ElementOfResult] {
+        return try flatMap(transform)
+    }
+}
+#endif
