1e242bea298f07ec21d4237a7b22190c7d940ca2
Theo <theophane.rupin@gmail.com>
2018-06-05 20:50:22 -0700

    Decomposed get & set in InstanceStore

--- Tests/WeaverDITests/InstanceCacheTests.swift
+++ Tests/WeaverDITests/InstanceCacheTests.swift
@@ -12,53 +12,53 @@ import XCTest
 
 final class InstanceCacheSpecs: XCTestCase {
     
-    var instances: InstanceCache!
+    var instances: InstanceStore!
     let instanceKey = InstanceKey(for: InstanceStub.self, name: "test")
     
     override func setUp() {
         super.setUp()
         
-        instances = InstanceCache()
+        instances = InstanceStore()
     }
     
     // When scope is `weak`.
     
-    func test_cache_should_release_weak_references_when_its_not_being_hold_anymore() {
+    func test_set_should_release_weak_references_when_its_not_being_hold_anymore() {
         
         var instanceStub: InstanceStub? = InstanceStub()
         weak var weakInstanceStub: InstanceStub? = instanceStub
         
-        _ = instances.cache(for: instanceKey, scope: .weak) { instanceStub }
+        _ = instances.set(for: instanceKey, scope: .weak) { instanceStub }
         
         instanceStub = nil
         
         XCTAssertNil(weakInstanceStub)
     }
     
-    func test_cache_should_build_the_instance_only_if_the_previous_instance_has_been_released() {
+    func test_set_should_build_the_instance_only_if_the_previous_instance_has_been_released() {
         
-        let firstInstance = instances.cache(for: instanceKey, scope: .weak) { InstanceStub() }
-        let secondInstance = instances.cache(for: instanceKey, scope: .weak) { InstanceStub() }
+        let firstInstance = instances.set(for: instanceKey, scope: .weak) { InstanceStub() }
+        let secondInstance = instances.set(for: instanceKey, scope: .weak) { InstanceStub() }
 
         XCTAssertEqual(firstInstance, secondInstance)
     }
     
     // When scope is `transient`.
 
-    func test_cache_should_create_a_new_reference_when_scope_is_transient() {
+    func test_set_should_create_a_new_reference_when_scope_is_transient() {
         
-        let firstInstance = instances.cache(for: instanceKey, scope: .transient) { InstanceStub() }
-        let secondInstance = instances.cache(for: instanceKey, scope: .transient) { InstanceStub() }
+        let firstInstance = instances.set(for: instanceKey, scope: .transient) { InstanceStub() }
+        let secondInstance = instances.set(for: instanceKey, scope: .transient) { InstanceStub() }
 
         XCTAssertNotEqual(firstInstance, secondInstance)
     }
     
-    func test_cache_should_not_hold_the_reference_when_scope_is_transient() {
+    func test_set_should_not_hold_the_reference_when_scope_is_transient() {
 
         var instanceStub: InstanceStub? = InstanceStub()
         weak var weakInstanceStub: InstanceStub? = instanceStub
         
-        _ = instances.cache(for: instanceKey, scope: .transient) { instanceStub }
+        _ = instances.set(for: instanceKey, scope: .transient) { instanceStub }
         
         instanceStub = nil
         
@@ -67,10 +67,10 @@ final class InstanceCacheSpecs: XCTestCase {
     
     // When scope is `graph`.
     
-    func test_cache_should_build_the_instance_only_once_when_scope_is_graph() {
+    func test_set_should_build_the_instance_only_once_when_scope_is_graph() {
 
-        let firstInstance = instances.cache(for: instanceKey, scope: .graph) { InstanceStub() }
-        let secondInstance = instances.cache(for: instanceKey, scope: .graph) { InstanceStub() }
+        let firstInstance = instances.set(for: instanceKey, scope: .graph) { InstanceStub() }
+        let secondInstance = instances.set(for: instanceKey, scope: .graph) { InstanceStub() }
         
         XCTAssertEqual(firstInstance, secondInstance)
     }
