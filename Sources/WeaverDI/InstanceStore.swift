5e435a709017f0924269e87815c6da972e5bebb9
Theo <theophane.rupin@gmail.com>
2018-06-06 21:10:44 -0700

    [#18] Added SynchronizedInstanceStore to ensure resolution thread safety

--- Sources/WeaverDI/InstanceStore.swift
+++ Sources/WeaverDI/InstanceStore.swift
@@ -11,27 +11,7 @@ import Foundation
 /// The cache strategy is based on the `scope`.
 protocol InstanceStoring {
     
-    func set<T>(value: (instance: T, scope: Scope), for key: InstanceKey)
-    
-    func get<T>(for key: InstanceKey) -> T?
-}
-
-extension InstanceStoring {
-    
-    func set<T>(for key: InstanceKey, scope: Scope, builder: () -> T) -> T {
-        
-        guard !scope.isTransient else {
-            return builder()
-        }
-
-        if let cachedInstance: T = get(for: key) {
-            return cachedInstance
-        }
-        
-        let instance = builder()
-        set(value: (instance, scope), for: key)
-        return instance
-    }
+    func set<T>(for key: InstanceKey, scope: Scope, builder: () -> T) -> T
 }
 
 // MARK: - Implementation
@@ -40,17 +20,23 @@ final class InstanceStore: InstanceStoring {
     
     private var instances: [InstanceKey: InstanceBox] = [:]
     
-    func set<T>(value: (instance: T, scope: Scope), for key: InstanceKey) {
-        clearReleasedInstances()
-        let box = InstanceBox(value.instance as AnyObject, scope: value.scope)
-        instances[key] = box
-    }
-    
-    func get<T>(for key: InstanceKey) -> T? {
-        guard let instance = instances[key]?.instance else {
-            return nil
+    func set<T>(for key: InstanceKey, scope: Scope, builder: () -> T) -> T {
+
+        guard !scope.isTransient else {
+            return builder()
         }
-        return instance as? T
+
+        clearReleasedInstances()
+        
+        if let cachedInstance = instances[key]?.instance as? T {
+            return cachedInstance
+        }
+        
+        let instance = builder()
+        let instanceBox = InstanceBox(instance as AnyObject, scope: scope)
+        instances[key] = instanceBox
+
+        return instance
     }
 }
 
@@ -76,19 +62,35 @@ private extension InstanceStore {
         private weak var weakInstance: AnyObject?
         private var strongInstance: AnyObject?
         
-        let scope: Scope
-        
         init(_ instance: AnyObject, scope: Scope) {
             if scope.isWeak {
                 weakInstance = instance
             } else {
                 strongInstance = instance
             }
-            self.scope = scope
         }
         
         var instance: AnyObject? {
             return weakInstance ?? strongInstance ?? nil
         }
+    }
+}
+
+// MARK: - SynchronizedInstanceStore
+
+final class SynchronizedInstanceStore: InstanceStoring {
+    
+    private let instances: InstanceStoring
+    
+    private let lock = NSLock()
+    
+    init(_ instances: InstanceStoring) {
+        self.instances = instances
+    }
+    
+    func set<T>(for key: InstanceKey, scope: Scope, builder: () -> T) -> T {
+        lock.lock()
+        defer { lock.unlock() }
+        return instances.set(for: key, scope: scope, builder: builder)
     }
 }
