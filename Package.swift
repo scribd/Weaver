7efadd1ee42fe97586d1d9bad1d411cd7f25784e
Theo <theophane.rupin@gmail.com>
2018-08-03 09:09:42 -0700

    [#57] Updated StencilSwiftKit to fix a compilation issue with Swift 4.2

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.21.0"),
         .package(url: "https://github.com/kylef/Commander.git", from: "0.6.0"),
         .package(url: "https://github.com/kylef/Stencil.git", from: "0.11.0"),
-        .package(url: "https://github.com/SwiftGen/StencilSwiftKit.git", from: "2.4.0")
+        .package(url: "https://github.com/SwiftGen/StencilSwiftKit.git", from: "2.5.0")
     ],
     targets: [
         .target(name: "WeaverCodeGen", dependencies: ["SourceKittenFramework", "Stencil", "StencilSwiftKit"]),
