523d97f75b42d4cf643f41c94d1b1aef103e34ef
Theo <theophane.rupin@gmail.com>
2018-09-25 10:55:41 -0700

    [#63] Updated dependencies to support Swift 4.2

--- Package.swift
+++ Package.swift
@@ -4,10 +4,10 @@ import PackageDescription
 let package = Package(
     name: "Weaver",
     dependencies: [
-        .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.21.0"),
-        .package(url: "https://github.com/kylef/Commander.git", from: "0.6.0"),
-        .package(url: "https://github.com/kylef/Stencil.git", from: "0.11.0"),
-        .package(url: "https://github.com/SwiftGen/StencilSwiftKit.git", from: "2.4.0")
+        .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.21.2"),
+        .package(url: "https://github.com/kylef/Commander.git", from: "0.8.0"),
+        .package(url: "https://github.com/kylef/Stencil.git", from: "0.12.1"),
+        .package(url: "https://github.com/SwiftGen/StencilSwiftKit.git", from: "2.6.0")
     ],
     targets: [
         .target(name: "WeaverCodeGen", dependencies: ["SourceKittenFramework", "Stencil", "StencilSwiftKit"]),
