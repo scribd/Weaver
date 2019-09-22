621a0aebcf363489a015cd53d94ebb04ed350057
Theo <theophane.rupin@gmail.com>
2019-09-22 11:06:27 -0700

    Force using Commander 0.8.0 as 0.9.0 injects a bug with Stencil

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.23.0"),
-        .package(url: "https://github.com/kylef/Commander.git", from: "0.9.0"),
+        .package(url: "https://github.com/kylef/Commander.git", "0.7.0"..."0.8.0"),
         .package(url: "https://github.com/kylef/Stencil.git", from: "0.12.1"),
         .package(url: "https://github.com/SwiftGen/StencilSwiftKit.git", from: "2.6.0"),
         .package(url: "https://github.com/onevcat/Rainbow", from: "3.0.0"),
