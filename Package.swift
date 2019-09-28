7211d87518371090127340817e2b77be5fedd3d6
Theo <theophane.rupin@gmail.com>
2019-09-28 08:30:30 -0700

    [#116] Added Meta and removed Stencil

--- Package.swift
+++ Package.swift
@@ -9,14 +9,13 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.23.0"),
         .package(url: "https://github.com/kylef/Commander.git", "0.7.0"..."0.8.0"),
-        .package(url: "https://github.com/kylef/Stencil.git", from: "0.12.1"),
-        .package(url: "https://github.com/SwiftGen/StencilSwiftKit.git", from: "2.6.0"),
         .package(url: "https://github.com/onevcat/Rainbow", from: "3.0.0"),
         .package(url: "https://github.com/jpsim/Yams.git", from: "2.0.0"),
-        .package(url: "https://github.com/kylef/PathKit.git", from: "0.9.0")
+        .package(url: "https://github.com/kylef/PathKit.git", from: "0.9.0"),
+        .package(url: "https://github.com/scribd/Meta.git", .branch("master"))
     ],
     targets: [
-        .target(name: "WeaverCodeGen", dependencies: ["SourceKittenFramework", "Stencil", "StencilSwiftKit"]),
+        .target(name: "WeaverCodeGen", dependencies: ["SourceKittenFramework", "Meta"]),
         .testTarget(name: "WeaverCodeGenTests", dependencies: ["WeaverCodeGen"]),
         .target(name: "WeaverCommand", dependencies: ["PathKit", "Commander", "Rainbow", "Yams", "WeaverCodeGen"])
     ]
